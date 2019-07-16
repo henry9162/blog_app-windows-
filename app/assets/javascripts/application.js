e91e51680d8d62867156504942fdce706f838151
somto amaugo <amaugosomto@gmail.com>
2019-07-16 12:09:25 +0100

    stylings added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require rails-ujs
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
